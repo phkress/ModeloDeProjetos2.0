70d76619d7291d2b22209e2108557d2a1fbc8728
Pedro Kress <phkress@gmail.com>
2019-06-28 13:55:34 -0300

    Create and configurate Vue-project

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# ModeloDeProjetos2.0
-Sistema
+# modelodeprojetos2.0
+
+> Gerenciador de Projetos da YIPI
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+// import Formulario from './components/formulario/Formulario.vue';
+// import Formularios from './components/formularios/Formularios.vue';
+// import Home from './components/home/Home.vue';
+// import Foto from './components/foto/Foto.vue';
+// import Login from './components/login/Login.vue';
+export const routes = [
+  // {path: '', name:'home', component: Formularios, titulo: 'Home', menu: true},
+  // {path: '/login', name:'Login', component: Login,  titulo: 'Login', menu: false},
+  // {path: '/formulario', name:'formulario', component: Formulario,  titulo: 'Formulario', menu: false},
+  // {path: '/formularios', name:'formularios', component: Formularios,  titulo: 'Formularios', menu: false},
+  // {path: '/foto', name:'foto', component: Foto,  titulo: 'Foto', menu: false},
+  // {path: '*', name:'homeGeral', component: Formularios, titulo: 'Home', menu: false},
+];
